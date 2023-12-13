d2ff7e7864c5767db0c700f9246181702a4a7cbf
Ryglus <jirka16.kucera@gmail.com>
2023-12-13 13:25:28 +0100

    Implement new feature XYZ

---
+++ src/components/Cat.js
@@ -0,0 +1,12 @@
+import { Avatar } from '@mui/material';
+import React, { Component } from 'react';
+
+const Cat = () => {
+    return (
+        <div>
+            <Avatar src="https://i.pinimg.com/736x/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"/>
+        </div>
+    );
+}
+
+export default Cat;
